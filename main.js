4a43f08d4e33bb9e652d0146a6c074e1ed809428
梁晓谊 <758915145@qq.com>
2020-07-20 15:58:25 +0800

    - 初步完成

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ var { createProxyMiddleware} = require('http-proxy-middleware');
 var app = express();
 
 app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  res.header('Access-Control-Allow-Methods', '*')
+  res.header('Content-Type', 'application/json;charset=UTF-8')
   if (req.method.toLowerCase() == 'options') {
     res.sendStatus(200);  // 让options尝试请求快速结束
     return
@@ -15,7 +16,12 @@ app.all('*', function (req, res, next) {
   if (target) {
     createProxyMiddleware({
       target: target,
-      changeOrigin: true
+      changeOrigin: true,
+      onProxyReq: function (proxyReq, req, res) {
+        proxyReq.removeHeader('cors-origin')
+        proxyReq.removeHeader('Origin')
+        return proxyReq
+      }
     })(req, res, next)
   } else {
     res.send('{code: -1, message: "添加cors-origin请求头才能跨域，如：cors-origin:\"https://www.baidu.com\""}')
